a86da2cb58f82f792d7bc9af3467d05c10eb708a
kerochama <thekerochama@gmail.com>
2021-09-26 12:34:27 -0400

    add ReplyCache

--- mushmom/utils/errors.py
+++ mushmom/utils/errors.py
@@ -5,6 +5,7 @@ Custom errors for bot
 import discord
 
 from discord.ext import commands
+from datetime import datetime
 
 from mushmom import config
 
@@ -45,20 +46,39 @@ send = send_error  # alias
 
 
 class ReplyCache:
-    def __init__(self):
+    def __init__(self, garbage_collect_after=50, timeout=300):
+        """
+        Maintains a cache of messages sent by bot in response to a command
+        so that they can be referenced/cleaned subsequently
+
+        :param garbage_collect_after: run garbage collect after n registers
+        :param timeout: seconds passed signifying delete
+        """
         # keep track of message replies to clean up when error
         # does not have to be a direct reply, just response to command
         self._reply_cache = {
-            # message.id: [replies]
+            # message.id: [(reply, ts)]
         }
+        self.garbage_collect_after = garbage_collect_after
+        self.timeout = timeout  # seconds
+        self._n = 0
 
     def register(self, ctx, reply):
         msg_id = ctx.message.id
+        record = (reply, datetime.utcnow())
 
         if msg_id in self._reply_cache:
-            self._reply_cache[msg_id].append(reply)
+            self._reply_cache[msg_id].append(record)
         else:
-            self._reply_cache[msg_id] = [reply]
+            self._reply_cache[msg_id] = [record]
+
+        self._n += 1
+
+        # run garbage collector
+        if (self.garbage_collect_after
+                and self._n >= self.garbage_collect_after):
+            self.run_garbage_collector()
+            self._n = 0
 
     def unregister(self, ctx):
         return self._reply_cache.pop(ctx.message.id, None)
@@ -71,16 +91,35 @@ class ReplyCache:
         :param delete:
         :return:
         """
-        replies = self.unregister(ctx) or []
+        replies = self.unregister(ctx.message.id) or []
 
         if not delete:
             return
 
-        for reply in replies:
+        for reply, ts in replies:
             try:
                 reply.delete()
             except (discord.Forbidden,discord.NotFound, discord.HTTPException):
                 pass
+
+    def run_garbage_collector(self):
+        """
+        Loop through cache and remove anything older than timeout
+
+        :return:
+        """
+        # clean each list
+        for msg_id, replies in self._reply_cache.items():
+            cleansed = [(reply, ts) for reply, ts in replies
+                        if (datetime.utcnow() - ts).seconds <= self.timeout]
+            self._reply_cache[msg_id] = cleansed
+
+        # clean dict
+        for msg_id in list(self._reply_cache.keys()):
+            if not self._reply_cache[msg_id]:  # empty list
+                del self._reply_cache[msg_id]
+
+        print('foo')
 
 
 class DataNotFound(commands.CommandError):
@@ -107,3 +146,9 @@ class TimeoutError(commands.CommandError):
     pass
 
 
+class NoMoreItems(commands.CommandError):
+    """
+    Command error version of discord.NoMoreItems
+
+    """
+    pass
