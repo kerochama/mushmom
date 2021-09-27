ccca9d010aa9e4239c705f94649f279f1450e0ed
kerochama <thekerochama@gmail.com>
2021-09-26 23:49:10 -0400

    allow pass session

--- mushmom/mapleio/api.py
+++ mushmom/mapleio/api.py
@@ -1,4 +1,5 @@
 import aiohttp
+import functools
 
 from PIL import Image
 from io import BytesIO
@@ -6,52 +7,72 @@ from io import BytesIO
 from mushmom import config
 
 
-async def latest_version(region='GMS'):
+def with_session(coro):
+    """
+    Decorator to handle adding aiohttp session if not provided
+
+    :param coro:
+    :return:
+    """
+    @functools.wraps(coro)
+    async def wrapper(*args, **kwargs):
+        if 'session' not in kwargs or kwargs['session'] is None:
+            kwargs.pop('session', None)
+            async with aiohttp.ClientSession() as session:
+                ret = await coro(session=session, *args, **kwargs)
+        else:
+            ret = await coro(*args, **kwargs)
+        return ret
+    return wrapper
+
+
+@with_session
+async def latest_version(region='GMS', session=None):
     """
     Get the latest version for region
 
     :param region:
+    :param session:
     :return:
     """
     u = f'{config.MAPLEIO_API}/wz'
 
-    # http request
-    async with aiohttp.ClientSession() as session:
-        async with session.get(u) as r:
-            if r.status == 200:
-                data = await r.json()
-                region_data = [x for x in data if x['isReady'] and x['region'] == region]
-                latest = region_data[-1]['mapleVersionId']
-            else:
-                latest = '225'  # arbitrary
+    async with session.get(u) as r:
+        if r.status == 200:
+            data = await r.json()
+            region_data = [x for x in data if x['isReady'] and x['region'] == region]
+            latest = region_data[-1]['mapleVersionId']
+        else:
+            latest = '225'  # arbitrary
 
     return latest
 
 
+@with_session
 async def get_item(itemid, region='GMS',
-                   version=config.MAPLEIO_DEFAULT_VERSION):
+                   version=config.MAPLEIO_DEFAULT_VERSION,
+                   session=None):
     """
     Get info about itemid
 
     :param itemid:
     :param region:
     :param version:
+    :param session:
     :return:
     """
     u = f'{config.MAPLEIO_API}/{region}/{version}/item/{itemid}'
+
     # http request
-    async with aiohttp.ClientSession() as session:
-        async with session.get(u) as r:
-            if r.status == 200:
-                data = await r.json()
-            else:
-                data = None
-
-    return data
+    async with session.get(u) as r:
+        if r.status == 200:
+            return await r.json()
 
 
+@with_session
 async def get_sprite(char, pose='stand1', emotion='default',
-                     zoom=1, flipx=False, bgcolor=(0, 0, 0, 0)):
+                     zoom=1, flipx=False, bgcolor=(0, 0, 0, 0),
+                     session=None):
     """
     Make API call to get char sprite data
 
@@ -61,20 +82,22 @@ async def get_sprite(char, pose='stand1', emotion='default',
     :param zoom:
     :param flipx:
     :param bgcolor:
+    :param session:
     :return:
     """
     args = locals().copy()
     args.pop('char')
+    args.pop('session')
     u = char.url(**args)
 
     # http request
-    async with aiohttp.ClientSession() as session:
-        async with session.get(u) as r:
-            if r.status == 200:
-                return await r.read()  # png bytes
+    async with session.get(u) as r:
+        if r.status == 200:
+            return await r.read()  # png bytes
 
 
-async def get_emote(char, emotion='default', zoom=1):
+@with_session
+async def get_emote(char, emotion='default', zoom=1, pad=8, session=None):
     """
     Make API call to get char sprite data, crop out body,
     and return bytes.  Remove cape and weapon
@@ -82,24 +105,23 @@ async def get_emote(char, emotion='default', zoom=1):
     :param char:
     :param emotion:
     :param zoom:
+    :param pad:
+    :param session:
     :return:
     """
-    args = locals().copy()
-    args.pop('char')
-    u = char.url(remove=['Cape', 'Weapon'], **args)
+    u = char.url(emotion=emotion, zoom=zoom, remove=['Cape', 'Weapon'])
 
-    async with aiohttp.ClientSession() as session:
-        async with session.get(u) as r:
-            if r.status == 200:
-                img_data = await r.read()  # png bytes
-            else:
-                return  # return None
+    async with session.get(u) as r:
+        if r.status == 200:
+            img_data = await r.read()  # png bytes
 
-    # crop body out image
-    img = Image.open(BytesIO(img_data))
-    w, h = img.size
-    pad = 8
-    emote = img.crop((0, 0, w, h + zoom * (pad - config.MAPLEIO_BODY_HEIGHT)))
-    byte_arr = BytesIO()
-    emote.save(byte_arr, format='PNG')
-    return byte_arr.getvalue()
+            # crop body out image
+            img = Image.open(BytesIO(img_data))
+            w, h = img.size
+
+            scaled_body_height = zoom * (config.MAPLEIO_BODY_HEIGHT - pad)
+            emote = img.crop((0, 0, w, h - scaled_body_height))
+            byte_arr = BytesIO()
+            emote.save(byte_arr, format='PNG')
+
+            return byte_arr.getvalue()
