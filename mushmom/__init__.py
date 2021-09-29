67dbb28d9a2b8f35da8afe66867327cc2d9cc3c9
kerochama <thekerochama@gmail.com>
2021-09-28 23:05:38 -0400

    use __package__

--- mushmom/__init__.py
+++ mushmom/__init__.py
@@ -2,11 +2,10 @@
 Parse config and make it accessible
 
 """
-import sys
 import munch
 
 from importlib import resources
 
 
-with resources.open_binary(sys.modules[__name__], 'config.yaml') as fp:
+with resources.open_binary(__package__, 'config.yaml') as fp:
     config = munch.Munch.fromYAML(fp)
